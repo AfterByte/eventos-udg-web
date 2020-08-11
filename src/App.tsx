b24ef4d7081de2dcbb5424e9f25b9d98f7e39b90
Josué <josueenrique1999@gmail.com>
2020-08-11 13:09:30 -0500

    React + Tailwind + Typescript setup ✨
    Created empty React project with TailwindCSS & Typescript initialized

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './assets/main.css'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <p>Hola Mundo</p>
+      </div>
+    )
+  }
+}
