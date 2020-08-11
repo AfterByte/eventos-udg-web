b1a3d0a3d1360ffc3fef70a08a797ab7d9a5d419
Reyes <reyes.rhernandez@alumnos.udg.mx>
2020-08-11 13:43:21 -0500

    Components create
    The components login,sidebar, header, menu, perfil, taskbar, eventcard. were created

---
+++ src/Components/TaskBar.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class TaskBar extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
