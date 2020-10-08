f807805c00d3ddb20fa92919ab5499bdaa1f180d
Josué <josueenrique1999@gmail.com>
2020-10-08 17:45:07 -0500

    Location Detail web fixed
    Location details forms were fixed.

--- src/Views/LocationDetails.tsx
+++ src/Views/LocationDetails.tsx
@@ -6,8 +6,6 @@ import DeleteMessage from "../Components/DeleteMessage";
 //google maps
 import GoogleMapReact from "google-map-react";
 import Marker from "../Components/Marker";
-// formik
-import { Formik, Field, Form, FormikHelpers } from "formik";
 //icons
 import Help from "../assets/icons/help.svg";
 import Search from "../assets/icons/search-black.svg";
@@ -82,223 +80,123 @@ export default function LocationDetails() {
 
         {!sidebarHidden ? (
           <div className="col-span-6 sm:col-start-2 sm:col-end-7 w-full h-full pt-24 sm:pt-24 z-0 pb-24 sm:pb-0 bg-indigo-500 bg-opacity-50">
-            <Formik
-              initialValues={{
-                name: location.name,
-                location: convertLocation(),
-                capacity: location.capacity,
-                isAllowed: false,
-                isEnabled: false,
-              }}
-              onSubmit={(
-                values: formValues,
-                { setSubmitting }: FormikHelpers<formValues>
-              ) => {
-                setTimeout(() => {
-                  alert(JSON.stringify(values, null, 2));
-                  setSubmitting(false);
-                }, 500);
-              }}
-            >
-              <div className="grid grid-cols-6 mt-4 xl:mt-4 sm:mt-6 mb-4 sm:mb-8 h-auto">
-                {/* Firts section */}
-                <div className="col-span-6 xl:col-span-4 grid grid-cols-6 gap-4 bg-white ml-2 xl:ml-4 mr-2 rounded-md shadow-sm">
-                  <div className="col-span-4 mt-2 ml-2">
-                    <Form className="grid grid-cols-1">
-                      <label htmlFor="name" className="pb-2">
-                        Nombre
-                      </label>
-                      <Field
-                        id="name"
-                        name="name"
-                        placeholder="Nombre"
-                        className="border border-gray-500 rounded-sm py-2 px-2"
-                      />
-                    </Form>
-                  </div>
-                  <div className="col-span-2 mt-2">
-                    <Form className="grid grid-cols-1">
-                      <label
-                        htmlFor="capacity"
-                        className="text-xs pb-3 xl:text-base xl:pb-2"
-                      >
-                        Capacidad máxima
-                      </label>
-                      <Field
-                        id="capacity"
-                        name="capacity"
-                        placeholder="Capacidad máxima"
-                        className="border border-gray-500 rounded-sm py-2 px-2 mr-2"
-                      />
-                    </Form>
-                  </div>
-                  <div className="col-span-6 ml-2">
-                    <Form className="grid grid-cols-1">
-                      <label htmlFor="location" className="pb-2">
-                        Ubicación
-                      </label>
-                      <Field
-                        id="location"
-                        name="location"
-                        placeholder="Ubicación"
-                        className="border border-gray-500 rounded-sm py-2 px-2 mr-2"
-                      />
-                    </Form>
-                  </div>
-                  {/* Google map */}
-                  <div className="col-span-6 ml-2 mr-2 h-64">
-                    <GoogleMapReact
-                      bootstrapURLKeys={{
-                        key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY as
-                          | string
-                          | "",
-                      }}
-                      defaultCenter={mapConfig.center}
-                      defaultZoom={mapConfig.zoom}
-                    >
-                      <Marker
-                        lat={location.lat}
-                        lng={location.lng}
-                        name="My Marker"
-                        color="#779BE7"
-                      />
-                    </GoogleMapReact>
-                  </div>
-
-                  <div className="col-span-6 ml-2">
-                    {location.isAllowed ? (
-                      <p className="text-lg">
-                        Esta ubicación si es administrada por UDG
-                      </p>
-                    ) : (
-                      <p className="text-lg">
-                        Esta ubicación no es administrada por UDG
-                      </p>
-                    )}
-                    <div className="grid grid-cols-12 my-4">
-                      <div className="col-span-1 flex justify-center pt-2">
-                        <img className="h-5" src={Help} alt="help-icon" />
-                      </div>
-                      <p className="col-span-8 text-sm">
-                        Ubicaciones no pertenecientes a UDG pueden agregarse en
-                        la agenda de Eventos de UDG pero se debe consultar
-                        primero al encargado del lugar.
-                      </p>
-                    </div>
-                  </div>
-
-                  <div className="col-span-6 ml-2">
-                    {location.isEnabled ? (
-                      <p className="text-lg">
-                        La ubicación se encuentra habilitada
-                      </p>
-                    ) : (
-                      <p className="text-lg">
-                        La ubicación se encuentra deshabilitada temporalmente
-                      </p>
-                    )}
-                    <div className="grid grid-cols-12 my-4">
-                      <div className="col-span-1 flex justify-center">
-                        <img className="h-5" src={Help} alt="help-icon" />
-                      </div>
-                      <p className="col-span-8 text-sm">
-                        Cuando la ubicación se encuentra deshabilitada, está no
-                        puede utilizarse en ningún evento nuevo.
-                      </p>
-                    </div>
-                  </div>
-
-                  {/* buttons */}
-                  <div className="col-span-6 ml-2 mt-20 mb-8 grid grid-cols-12 gap-4">
-                    <div className="col-span-12 xl:col-span-3 text-center">
-                      <Link to="/locations">
-                        <button className="bg-gray-500 text-white font-medium py-1 px-12 rounded-md transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-100">
-                          Volver
-                        </button>
-                      </Link>
-                    </div>
-                    <div className="col-span-12 xl:col-span-6 text-center">
-                      <Link to={`/editLocation/${location.id}`}>
-                        <button className="bg-blue-500 text-white text-xl font-medium py-1 px-12 rounded-md transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-100">
-                          Editar
-                        </button>
-                      </Link>
-                    </div>
-                    <div className="col-span-12 xl:col-span-3 text-center">
-                      <button
-                        onClick={() => setShowMessage(true)}
-                        className="bg-red-500 text-white font-medium py-1 px-12 rounded-md transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-100"
-                      >
-                        Eliminar
-                      </button>
+            
+            <div className="grid grid-cols-6 mt-4 xl:mt-4 sm:mt-6 mb-4 sm:mb-8 h-auto">
+              {/* Firts section */}
+              <div className="col-span-6 xl:col-span-4 grid grid-cols-6 gap-4 bg-white ml-2 xl:ml-4 mr-2 rounded-md shadow-sm">
+                <div className="col-span-4 mt-2 ml-2">
+                  <div className="grid grid-cols-1">
+                    <label htmlFor="name" className="pb-2">Nombre</label>
+                    <div className="border border-gray-500 rounded-sm py-2 px-2 mr-2">
+                      <p>{location.name}</p>
                     </div>
                   </div>
                 </div>
-
-                {/* Second section */}
-                <div className="col-span-6 xl:col-span-2 bg-white ml-2 mr-2 xl:mr-4 rounded-md shadow-sm mt-4 xl:mt-0 pb-4 xl:pb-0">
-                  <div className="col-span-2 grid grid-cols-6 mt-4 ml-2 mr-2">
-                    <div className="col-span-6 border-b-4">
-                      <p className="text-lg">
-                        Organizadores que pueden hacer uso de esta ubicación
-                      </p>
-                      <div className="grid grid-cols-12 my-4">
-                        <div className="col-span-1 flex justify-center pt-2">
-                          <img className="h-5" src={Help} alt="help-icon" />
-                        </div>
-                        <p className="col-span-10 text-sm">
-                          Solo los organizadores de las universidades aquí
-                          indicadas podrán hacer uso de esta ubicación al crear
-                          un evento.
-                        </p>
-                      </div>
+                <div className="col-span-2 mt-2">
+                  <div className="grid grid-cols-1">
+                    <label htmlFor="capacity" className="text-xs pb-3 xl:text-base xl:pb-2">Capacidad máxima</label>
+                    <div className="border border-gray-500 rounded-sm py-2 px-2 mr-2">
+                      <p>{location.capacity}</p>
                     </div>
                   </div>
-
-                  <div className="col-span-2 grid grid-cols-6 mt-4 ml-2 mr-2">
-                    <div className="col-start-3 col-end-7 flex ml-2 sm:ml-0 justify-center sm:justify-end">
-                      <form
-                        className="flex w-full"
-                        onSubmit={(e) => e.preventDefault()}
-                      >
-                        <label className="flex w-full h-full mr-2 sm:mr-0 border border-gray-500 border-opacity-50">
-                          <img
-                            className="h-full sm:py-0 py-1 px-1 bg-gray-500 bg-opacity-50"
-                            style={{ opacity: "0.5" }}
-                            src={Search}
-                            alt={"SearchIcon"}
-                          />
-                          <input
-                            className="sm:mr-0 px-1 bg-gray-500 bg-opacity-25 w-full"
-                            type="text"
-                            name="buscar"
-                          />
-                        </label>
-                        <div className="hidden sm:grid mr-2 px-2 py-2 text-md font-normal text-white bg-blue-500 items-center">
-                          Buscar
-                        </div>
-                      </form>
+                </div>
+                <div className="col-span-6 ml-2">
+                  <div className="grid grid-cols-1">
+                    <label htmlFor="location" className="pb-2">Ubicación</label>
+                    <div className="border border-gray-500 rounded-sm py-2 px-2 mr-2">
+                      <p>{convertLocation()}</p>
                     </div>
                   </div>
+                </div>
+                {/* Google map */}
+                <div className="col-span-6 ml-2 mr-2 h-64" >
+                  <GoogleMapReact
+                    bootstrapURLKeys={{
+                      key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY as string | "",
+                    }}
+                    defaultCenter={mapConfig.center}
+                    defaultZoom={mapConfig.zoom}
+                  >
+                    <Marker
+                      lat={location.lat}
+                      lng={location.lng}
+                      name="My Marker"
+                      color="#779BE7"
+                    />
+                  </GoogleMapReact>
+                </div>
 
-                  {/* organizers list */}
-                  <div className="col-span-2 grid grid-cols-6 gap-4 mt-6 ml-2 mr-2">
-                    {location.organizers.map((organizers) => (
-                      <div className="col-span-3 py-2 flex justify-between items-center border border-gray-500">
-                        <p className="text-center pl-2">{organizers}</p>
-                        <button>
-                          <img
-                            className="h-6"
-                            src={Clear}
-                            alt={"ClearTagIcon"}
-                          />
-                        </button>
-                      </div>
-                    ))}
+                <div className="col-span-6 ml-2">
+                  {location.isAllowed
+                  ? <p className="text-lg">Esta ubicación si es administrada por UDG</p>
+                  : <p className="text-lg">Esta ubicación no es administrada por UDG</p>}
+                  <div className="grid grid-cols-12 my-4">
+                    <div className="col-span-1 flex justify-center pt-2"><img className="h-5" src={Help} alt="help-icon"/></div>
+                    <p className="col-span-8 text-sm">Ubicaciones no pertenecientes a UDG pueden agregarse en la agenda de Eventos de UDG pero se debe consultar primero al encargado del lugar.</p>
+                  </div>
+                </div>
+
+                <div className="col-span-6 ml-2">
+                  {location.isEnabled
+                  ? <p className="text-lg">La ubicación se encuentra habilitada</p>
+                  : <p className="text-lg">La ubicación se encuentra deshabilitada temporalmente</p>}
+                  <div className="grid grid-cols-12 my-4">
+                    <div className="col-span-1 flex justify-center"><img className="h-5" src={Help} alt="help-icon"/></div>
+                    <p className="col-span-8 text-sm">Cuando la ubicación se encuentra deshabilitada, está no puede utilizarse en ningún evento nuevo.</p>
+                  </div>
+                </div>
+
+                {/* buttons */}
+                <div className="col-span-6 ml-2 mt-20 mb-8 grid grid-cols-12 gap-4">
+                  <div className="col-span-12 xl:col-span-3 text-center">
+                    <Link to="/locations"><button className="bg-gray-500 text-white font-medium py-1 px-12 rounded-md transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-100">Volver</button></Link>
+                  </div>
+                  <div className="col-span-12 xl:col-span-6 text-center">
+                    <Link to={`/editLocation/${location.id}`}><button className="bg-blue-500 text-white text-xl font-medium py-1 px-12 rounded-md transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-100">Editar</button></Link>
+                  </div>
+                  <div className="col-span-12 xl:col-span-3 text-center">
+                    <button onClick={() => setShowMessage(!showMessage)} className="bg-red-500 text-white font-medium py-1 px-12 rounded-md transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-100">Eliminar</button>
                   </div>
                 </div>
               </div>
-            </Formik>
+
+              {/* Second section */}
+              <div className="col-span-6 xl:col-span-2 bg-white ml-2 mr-2 xl:mr-4 rounded-md shadow-sm mt-4 xl:mt-0 pb-4 xl:pb-0">
+                <div className="col-span-2 grid grid-cols-6 mt-4 ml-2 mr-2">
+                  <div className="col-span-6 border-b-4">
+                    <p className="text-lg">Organizadores que pueden hacer uso de esta ubicación</p>
+                    <div className="grid grid-cols-12 my-4">
+                      <div className="col-span-1 flex justify-center pt-2"><img className="h-5" src={Help} alt="help-icon"/></div>
+                      <p className="col-span-10 text-sm">Solo los organizadores de las universidades aquí indicadas podrán hacer uso de esta ubicación al crear un evento.</p>
+                    </div>
+                  </div>
+                </div>
+              
+                <div className="col-span-2 grid grid-cols-6 mt-4 ml-2 mr-2">
+                  <div className="col-start-3 col-end-7 flex ml-2 sm:ml-0 justify-center sm:justify-end">
+                    <form className="flex w-full" onSubmit={(e) => e.preventDefault()}>
+                      <label className="flex w-full h-full mr-2 sm:mr-0 border border-gray-500 border-opacity-50">
+                        <img className="h-full sm:py-0 py-1 px-1 bg-gray-500 bg-opacity-50" style={{opacity: "0.5"}} src={Search} alt={"SearchIcon"}/>
+                        <input className="sm:mr-0 px-1 bg-gray-500 bg-opacity-25 w-full" type="text" name="buscar"/>
+                      </label>
+                      <div className="hidden sm:grid mr-2 px-2 py-2 text-md font-normal text-white bg-blue-500 items-center">Buscar</div>
+                    </form>
+                  </div>
+                </div>
+
+                {/* organizers list */}
+                <div className="col-span-2 grid grid-cols-6 gap-4 mt-6 ml-2 mr-2">
+                  {location.organizers.map((organizers) =>
+                    <div className="col-span-3 py-2 flex justify-between items-center border border-gray-500">
+                      <p className="text-center pl-2">{organizers}</p>
+                      <button><img className="h-6" src={Clear} alt={"ClearTagIcon"}/></button>
+                    </div>
+                  )}
+                </div>
+
+              </div>
+            </div>
+            
 
             {showMessage ? (
               <DeleteMessage
